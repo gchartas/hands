6fd8d76ccda19490abf9869d0f40c2902ca54e4a
gchartas <gchartasol@gmail.com>
2020-05-08 17:38:15 +0000

    New Doctype

---
+++ hands/system_management/doctype/management_systems/management_systems.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, EngLandGR and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Management Systems', {
+	// refresh: function(frm) {
+
+	// }
+});
