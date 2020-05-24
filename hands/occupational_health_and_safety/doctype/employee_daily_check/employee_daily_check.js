9a6ff80a599917ba930f8b0ad23bcd396f5429d3
gchartas <gchartasol@gmail.com>
2020-05-24 14:48:39 +0000

    First Edition.1

--- hands/occupational_health_and_safety/doctype/employee_daily_check/employee_daily_check.js
+++ hands/occupational_health_and_safety/doctype/employee_daily_check/employee_daily_check.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Daily Check', {
-	// refresh: function(frm) {
-
-	// }
+	template: function(frm){
+	    frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Employee Daily Check Template",
+				name: frm.doc.template
+			},
+			callback: function(data){
+				frm.fields_dict.parameters.grid.remove_all();
+				for (var i in data.message.parameters){
+					frm.add_child("parameters");
+					frm.fields_dict.parameters.get_value()[i].parameter = data.message.parameters[i].parameter;
+				}
+				frm.refresh();
+			}
+		});
+	}
 });
